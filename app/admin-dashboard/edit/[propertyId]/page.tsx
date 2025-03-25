06f77dcc72593310ead2fddce3e72b3c71c8a0ac
yoonju88 <yoonju.t@gmail.com>
2025-03-25 12:33:57 +0100

    fix issue of 'property' is possibly 'null'

--- app/admin-dashboard/edit/[propertyId]/page.tsx
+++ app/admin-dashboard/edit/[propertyId]/page.tsx
@@ -2,13 +2,20 @@ import { Breadcrumbs } from '@/components/ui/breadcrumb'
 import { getPropertyById } from '@/lib/properties';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import EditPropertyForm from './edit-property-form';
-import notFound from './not-found';
+
 
 export default async function EditProperty({ params }: { params: Promise<{ propertyId: string }> }) {
     const { propertyId } = await params
     const property = await getPropertyById(propertyId)
 
-    if (!property) { notFound() }
+    if (!property) {
+        return (
+            <div>
+                <h2>Property not found</h2>
+                <p>The property with the given ID could not be found.</p>
+            </div>
+        )
+    }
 
     return (
         <div>
