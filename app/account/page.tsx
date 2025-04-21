f934b1b4f2634a9434aaeb8dd432b1b057d3abaf
yoonju88 <yoonju.t@gmail.com>
2025-04-21 16:55:29 +0200

    Added the account page ans render user data

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,48 @@
+import { DecodedIdToken } from 'firebase-admin/auth';
+import { cookies } from 'next/headers'
+import { auth } from '@/firebase/server';
 import React from 'react'
+import { redirect } from 'next/navigation';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
 
-export default function page() {
+export default async function Account() {
+
+    const cookieStore = await cookies();
+    const token = cookieStore.get("firebaseAuthToken")?.value
+    //console.log(token)
+    if (!token) { redirect('/') }
+
+    let decodedToken: DecodedIdToken;
+    try {
+        decodedToken = await auth.verifyIdToken(token)
+    } catch {
+        redirect('/')
+    }
+
+    const user = auth.getUser(decodedToken.uid)
+    const isPasswordProvider = (await user).providerData.find(
+        (provider) => provider.providerId === "password"
+    )
+
     return (
-        <div>
-            Account pages
+        <div className='max-w-screen-md mx-auto '>
+            <Card className='mt-10'>
+                <CardHeader>
+                    <CardTitle className="text-3xl font-bold">
+                        My Account
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Label>Email</Label>
+                    <div>{decodedToken.email}</div>
+                    {!!isPasswordProvider && (
+                        <div>
+                            Update Password
+                        </div>
+                    )}
+                </CardContent>
+            </Card>
         </div>
     )
 }
